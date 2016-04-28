eb17e5422c3cab9a0f16aa0bfa49a6144b1699e4
ljf <ljf@LJF_DT>
2016-04-28 09:37:07 +0800

    Add project files.

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,32 @@
+#include "stdafx.h"
+#include <thread>
+#include <mutex>
+#include <iostream>
+#include <typeinfo>
+using namespace std;
+
+
+template <class Fn,class... Args>
+ void func(Fn&&f, Args&&... arg)
+{
+	f(arg...);
+}
+
+
+void f(int k)
+{
+	cout << "s:\n";
+}
+void f(double, double)
+{
+	cout << "dobule\n";
+}
+
+typedef void (*FT)(int)  ;
+
+
+
+void main()
+{
+
+}
